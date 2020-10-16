fb200546d06d37e266ec873e7c1ba9b7aff0ac49
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-10-16 09:51:31 -0500

    Updates

---
+++ precache-manifest.cd1a13130d4f61ee3b5b3a87d97c0dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c5c07ef3ef71752157d4354e3679dd",
+    "url": "/imagenes-del-mundo/index.html"
+  },
+  {
+    "revision": "66de5d17724d840097a3",
+    "url": "/imagenes-del-mundo/static/css/main.9f46a12e.chunk.css"
+  },
+  {
+    "revision": "8acfcad0198a8e0a8d9a",
+    "url": "/imagenes-del-mundo/static/js/2.6c269f05.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/imagenes-del-mundo/static/js/2.6c269f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66de5d17724d840097a3",
+    "url": "/imagenes-del-mundo/static/js/main.3ac5ed58.chunk.js"
+  },
+  {
+    "revision": "53085c2128d857938115",
+    "url": "/imagenes-del-mundo/static/js/runtime-main.69c90d7f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/imagenes-del-mundo/precache-manifest.cd1a13130d4f61ee3b5b3a87d97c0dc7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/imagenes-del-mundo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
